c2c2eb96d6c03f5d9bf1243817b341f75ca018e7
floydkelly-dev <floydkelly2012@gmail.com>
2025-08-05 23:26:17 -0700

    Create config.js
    Config

---
+++ config.js
@@ -0,0 +1,12 @@
+// This file contains your secret Firebase configuration.
+// IMPORTANT: This file should NOT be uploaded to GitHub.
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyDhYY3L2HI1Ovb2yNICs055M_7xrsR5uo4",
+  authDomain: "wordgreed-cc5eb.firebaseapp.com",
+  projectId: "wordgreed-cc5eb",
+  storageBucket: "wordgreed-cc5eb.appspot.com",
+  messagingSenderId: "395706904910",
+  appId: "1:395706904910:web:4675b1a9cc8b6cdf826e1f",
+  measurementId: "G-DSPDTH86X2"
+};
